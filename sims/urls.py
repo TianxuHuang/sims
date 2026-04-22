f6f231b5ee9b5f89eecf92d2c08d7e7254be7c50
TianxuHuang <422423782@qq.com>
2026-04-22 08:55:58 +0800

    feat(路由): 添加学生列表首页路由
    - 在根路由('')添加学生列表视图作为默认首页
    - 为路由配置添加中文注释说明
    - 保持原有媒体文件路由配置不变

--- sims/urls.py
+++ sims/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# 导入必要的模块
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from student import views
 
+# URL 路由配置列表
 urlpatterns = [
+    # 管理后台 URL
     path('admin/', admin.site.urls),
+    # 默认首页 - 显示学生列表
+    path('', views.list_student, name='home'),
+    # 学生应用的 URL 路由
     path('student/', include('student.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 静态媒体文件 URL 配置
